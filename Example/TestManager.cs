ae3e1cfd0b98cd16fb21f44cf1efa6eb67fbe933
kirinchen <a29243685@msn.com>
2017-10-05 22:36:25 +0800

    refacorry create Unity Fire Manager

--- Example/TestManager.cs
+++ Example/TestManager.cs
@@ -7,69 +7,46 @@ using Firebase.Database;
 using Firebase.Unity.Editor;
 
 namespace com.surfm.firebase.geofire.test {
-    public class TestManager : MonoBehaviour, GeoQueryEventListener {
+    public class TestManager : GeoFireManager {
 
         public LocationField putLoacation;
         public LocationField ceneterLocation;
-        private GeoFire geoFire;
-        private GeoQuery query;
-        DependencyStatus dependencyStatus = DependencyStatus.UnavailableOther;
+        //private GeoFire geoFire;
+        //private GeoQuery query;
+        //DependencyStatus dependencyStatus = DependencyStatus.UnavailableOther;
         void Start() {
-            dependencyStatus = FirebaseApp.CheckDependencies();
-            if (dependencyStatus != DependencyStatus.Available) {
-                FirebaseApp.FixDependenciesAsync().ContinueWith(task => {
-                    dependencyStatus = FirebaseApp.CheckDependencies();
-                    if (dependencyStatus == DependencyStatus.Available) {
-                        initializeFirebase();
-                    } else {
-                        Debug.LogError(
-                            "Could not resolve all Firebase dependencies: " + dependencyStatus);
-                    }
-                });
-            } else {
-                initializeFirebase();
-            }
-            ceneterLocation.onChangeAction = onCenterChange;
+            init(ceneterLocation.getLocation, () => {
+                Debug.Log("TestManager inited");
+                ceneterLocation.onChangeAction = onCenterChange;
+            });
         }
 
         private void onCenterChange() {
-            if (query != null) {
-                query.setCenter(ceneterLocation.getLocation());
-            }
+            onCenterLocationChange();
         }
-
-        private void initializeFirebase() {
-            FirebaseApp app = FirebaseApp.DefaultInstance;
-            app.SetEditorDatabaseUrl("https://apphi-224fb.firebaseio.com/");
-            if (app.Options.DatabaseUrl != null) app.SetEditorDatabaseUrl(app.Options.DatabaseUrl);
-            geoFire = new GeoFire(FirebaseDatabase.DefaultInstance.GetReference("/geo"));
-            query = geoFire.queryAtLocation(ceneterLocation.getLocation(), 2);
-            query.addGeoQueryEventListener(this);
-        }
-
-
 
         public void putPoint() {
             geoFire.setLocation("t" + Mathf.RoundToInt(Time.time * 1000), putLoacation.getLocation());
         }
 
-        public void onKeyEntered(string key, GeoLocation location) {
+        public override void onKeyEntered(string key, GeoLocation location) {
             Debug.Log(string.Format("onKeyEntered key={0} loc={1}", key, location));
         }
 
-        public void onKeyExited(string key) {
+        public override void onKeyExited(string key) {
             Debug.Log(string.Format("onKeyExited key={0} ", key));
         }
 
-        public void onKeyMoved(string key, GeoLocation location) {
+        public override void onKeyMoved(string key, GeoLocation location) {
             Debug.Log(string.Format("onKeyMoved key={0} loc={1}", key, location));
         }
 
-        public void onGeoQueryReady() {
+        public override void onGeoQueryReady() {
+            base.onGeoQueryReady();
             Debug.Log("onGeoQueryReady");
         }
 
-        public void onGeoQueryError(DatabaseError error) {
+        public override void onGeoQueryError(DatabaseError error) {
             Debug.Log("onGeoQueryError=" + error);
         }
     }
---
+++ Unity/GeoFireManager.cs
@@ -0,0 +1,80 @@
+﻿using Firebase;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Firebase.Unity.Editor;
+using Firebase.Database;
+
+namespace com.surfm.firebase.geofire {
+    public abstract class GeoFireManager : MonoBehaviour, GeoQueryEventListener {
+
+        public string geoFireUrl;
+        protected GeoFire geoFire;
+        private GeoQuery query;
+        private DependencyStatus dependencyStatus = DependencyStatus.UnavailableOther;
+        private List<Action> readyActions = new List<Action>();
+        public void addReadyAction(Action a) { if (readyActions == null) { a(); } else { readyActions.Add(a); } }
+        public bool readyed { get; private set; }
+        private Func<GeoLocation> centerFunc;
+
+        public void init(Func<GeoLocation> geoFunc, Action readyAction) {
+            if (geoFunc == null) throw new NullReferenceException("geoFunc is empty");
+            centerFunc = geoFunc;
+            initFirebase();
+            addReadyAction(readyAction);
+            onCenterLocationChange();
+
+        }
+
+        public void onCenterLocationChange() {
+            Debug.Log("onCenterLocationChange");
+            if (query == null) {
+                query = geoFire.queryAtLocation(centerFunc(), 1);
+                query.addGeoQueryEventListener(this);
+            } else {
+                query.setCenter(centerFunc());
+            }
+        }
+
+        private void initFirebase() {
+            dependencyStatus = FirebaseApp.CheckDependencies();
+            if (dependencyStatus != DependencyStatus.Available) {
+                FirebaseApp.FixDependenciesAsync().ContinueWith(task => {
+                    dependencyStatus = FirebaseApp.CheckDependencies();
+                    if (dependencyStatus == DependencyStatus.Available) {
+                        initializeFirebase();
+                    } else {
+                        Debug.LogError(
+                            "Could not resolve all Firebase dependencies: " + dependencyStatus);
+                    }
+                });
+            } else {
+                initializeFirebase();
+            }
+        }
+
+        internal virtual void initializeFirebase() {
+            FirebaseApp app = FirebaseApp.DefaultInstance;
+            app.SetEditorDatabaseUrl(geoFireUrl);
+            if (app.Options.DatabaseUrl != null) app.SetEditorDatabaseUrl(app.Options.DatabaseUrl);
+            geoFire = new GeoFire(FirebaseDatabase.DefaultInstance.GetReference("/geo"));
+            //  query = geoFire.queryAtLocation(ceneterLocation.getLocation(), 2);
+           // query.addGeoQueryEventListener(this);
+        }
+
+
+        public virtual void onGeoQueryReady() {
+            if (!readyed) {
+                readyed = true;
+                readyActions.ForEach(a => { a(); });
+                readyActions = null;
+            }
+        }
+
+        public abstract void onKeyEntered(string key, GeoLocation location);
+        public abstract void onKeyExited(string key);
+        public abstract void onKeyMoved(string key, GeoLocation location);
+        public abstract void onGeoQueryError(DatabaseError error);
+    }
+}
